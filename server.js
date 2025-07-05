3a6dc991beebff24b76f43a2c829811cb6d325a3
redmi <hmadaredmi7@gmail.com>
2025-07-05 21:21:50 +0300

    3rd commit

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-require('dotenv').config(); // تحميل متغيرات البيئة من ملف .env
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const swaggerSetup = require('./config/swagger');
-
+require("dotenv").config(); // تحميل متغيرات البيئة من ملف .env
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const swaggerSetup = require("./config/swagger");
 
 // استيراد المتحكمات
-const authRoutes = require('./routes/authRoutes');
-const authController = require('./controllers/authController');
-const electronicPartRoutes = require('./routes/electronicPartRoutes');
-const technicianRoutes = require('./routes/technicianRoutes');
-const accessoryRoutes = require('./routes/accessoryRoutes');
-const repairRoutes = require('./routes/repairRoutes');
+const authRoutes = require("./routes/authRoutes");
+const authController = require("./controllers/authController");
+const electronicPartRoutes = require("./routes/electronicPartRoutes");
+const technicianRoutes = require("./routes/technicianRoutes");
+const accessoryRoutes = require("./routes/accessoryRoutes");
+const repairRoutes = require("./routes/repairRoutes");
 
 const app = express();
 
@@ -21,22 +20,25 @@ connectDB();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors({
-    origin: '*', // السماح بجميع المصادر (أي domain)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // السماح بجميع الطرق
-    allowedHeaders: ['Content-Type', 'Authorization'], // السماح بجميع الهيدرات
-  }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://electronics-wheat.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 swaggerSetup(app);
 
 // حماية جميع المسارات باستخدام verifyToken
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api', authController.verifyToken);
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api", authController.verifyToken);
 
 // المسارات
-app.use('/api/parts', electronicPartRoutes);
-app.use('/api/technicians', technicianRoutes);
-app.use('/api/accessories', accessoryRoutes);
-app.use('/api/repairs', repairRoutes);
+app.use("/api/parts", electronicPartRoutes);
+app.use("/api/technicians", technicianRoutes);
+app.use("/api/accessories", accessoryRoutes);
+app.use("/api/repairs", repairRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
